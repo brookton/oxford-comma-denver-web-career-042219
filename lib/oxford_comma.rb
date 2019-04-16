e9ef1c5f9a3067bebf5eab278206f3c3b4c08d49
brookton <davidbrookton@gmail.com>
2019-04-16 11:24:53 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  end
+  if array.size == 2
+    new_arr = array.join(" and ")
+    return new_arr
+  end
+  if array.size >= 3
+    last = array.pop
+    array.join(", ") << ", and #{last}"
+  end
 end
